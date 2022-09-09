93c26d2ecdf1961c5818e824cd6e818e39da23f1
Deimos <xman.smcv@gmail.com>
2022-09-09 08:57:09 -0500

    Subiendo Examen

---
+++ CalidadT1/TestRomanos/UnitTest1.cs
@@ -0,0 +1,186 @@
+
+namespace TestRomanos
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(10, ob);
+        }
+        public void Test2()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(20, ob);
+        }
+        public void Test3()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(10, ob);
+        }
+
+        public void Test4()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(30, ob);
+        }
+
+        public void Test5()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(40, ob);
+        }
+
+        public void Test6()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(80, ob);
+        }
+
+        public void Test7()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(200, ob);
+        }
+
+        public void Test8()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(110, ob);
+        }
+
+        public void Test9()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(45, ob);
+        }
+
+        public void Test10()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(55, ob);
+        }
+
+        public void Test11()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(50, ob);
+        }
+
+        public void Test12()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(35, ob);
+        }
+
+        public void Test13()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(12, ob);
+        }
+
+        public void Test14()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(14, ob);
+        }
+
+        public void Test15()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(32, ob);
+        }
+
+        public void Test16()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(44, ob);
+        }
+
+        public void Test17()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(300, ob);
+        }
+
+        public void Test18()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(350, ob);
+        }
+
+        public void Test19()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(402, ob);
+        }
+
+        public void Test20()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(380, ob);
+        }
+
+        public void Test21()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(71, ob);
+        }
+
+        public void Test22()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(66, ob);
+        }
+        public void Test23()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(49, ob);
+        }
+
+        public void Test24()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(21, ob);
+        }
+
+        public void Test25()
+        {
+            var nromano = RomanoGenerator();
+            var ob = nromano.GetRomano(100);
+            Assert.AreEqual(16, ob);
+        }
+
+    }
+}
